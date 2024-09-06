7535f3f040f30003b0f30c7367e649d769034b4f
Laurentino <rafael.laurentino@hotmail.com>
2024-09-06 20:42:52 -0300

    Aula 3

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou fãs ao redor do mundo.",
+        link: "https://olympics.com/pt/atletas/rayssa-leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro. Sua técnica apurada e determinação a colocaram como uma das principais promessas do judô brasileiro.",
+        link: "https://olympics.com/pt/atletas/beatriz-souza"
+    },
+        {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação.",
+        link: "https://olympics.com/pt/atletas/rebeca-andrade"
+
+    }
+];
+
