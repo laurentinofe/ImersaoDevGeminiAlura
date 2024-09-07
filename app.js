c2858d47a0e0ae7205258786bbfe104f0ad59d08
laurentinofe <rafael.laurentino@hotmail.com>
2024-09-07 14:17:56 -0300

    aula 5

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-    // Imprime a seção no console para fins de depuração (opcional)
-    console.log(section);
-  
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-  
-    // Itera sobre os dados e cria as divs dos resultados
-    for (let dado of dados) {
-      // Cria uma div com as informações do resultado
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  // Obtém o valor digitado no campo de pesquisa e o converte para minúsculas
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+  // Verifica se o campo de pesquisa está vazio
+  if (!campoPesquisa) {
+    // Se estiver vazio, exibe uma mensagem de erro
+    return section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar um parametro para busca.</p>";
+  }
+
+  // Inicializa a string que armazenará os resultados da pesquisa
+  let resultados = "";
+
+  // Itera sobre os dados a serem pesquisados
+  for (let dado of dados) {
+    // Converte o título, descrição e tags para minúsculas para facilitar a comparação
+    let titulo = dado.titulo.toLowerCase();
+    let descricao = dado.descricao.toLowerCase();
+    let tags = dado.tags.toLowerCase();
+
+    // Verifica se o termo de pesquisa está presente no título, descrição ou tags
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Se o termo for encontrado, adiciona um novo resultado à string
       resultados += `
         <div class="item-resultado">
           <h2>
@@ -20,7 +34,14 @@ function pesquisar() {
         </div>
       `;
     }
-  
-    // Atualiza o conteúdo HTML da seção com os resultados
-    section.innerHTML = resultados;
   }
+
+  // Verifica se algum resultado foi encontrado
+  if (!resultados) {
+    // Se nenhum resultado for encontrado, exibe uma mensagem
+    resultados = "<p>Nada foi encontrado</p>";
+  }
+
+  // Atualiza o conteúdo da seção com os resultados da pesquisa
+  section.innerHTML = resultados;
+}
